2302eff5574fe87a010796b1eb1729ff3ab5c5ee
Taras <tarikpulseup@gmail.com>
2020-03-10 13:48:12 +0200

    change status todo

--- todo.js
+++ todo.js
@@ -9,13 +9,11 @@ const TODOS = [
     }
 ];
 
-/* INIT TO WORK OBJ */
-let todos = TODOS;
-
-
-/* GET OBJ FROM STORAGE */
-let concatTodos = localStorage.getItem('todos') ?
+/* INIT TO WORK OBJ & GET OBJ FROM STORAGE */
+let todos       = TODOS,
+    concatTodos = localStorage.getItem('todos') ?
     JSON.parse(localStorage.getItem('todos')) : todos;
+
 
 /* SET TO STORAGE */
 localStorage.setItem('todos', JSON.stringify(concatTodos));
@@ -38,21 +36,79 @@ function getMaxId(arr) {
 }
 
 
+/* CHECK STATUS */
+function checkStat() {
+    let checkbox = document.querySelectorAll('.checkbox');
+
+    let newArr = [];
+
+    for (let check of checkbox) {
+        if (check.checked === true) {
+            concatTodos.filter(function (todo) {
+                todo.isDone = true;
+                newArr.push(todo.isDone);
+                console.log(todo.isDone);
+                localStorage.setItem('newArr', JSON.stringify(newArr));
+            });
+        } else {
+            concatTodos.filter(function (todo) {
+                todo.isDone = false;
+                newArr.push(todo.isDone);
+                console.log(todo.isDone);
+                localStorage.setItem('newArr', JSON.stringify(newArr));
+            });
+        }
+
+    }
+
+    newArr += localStorage.setItem('todos', JSON.stringify(concatTodos));
+
+
+
+    // concatTodos.filter(function (todo) {
+    //     console.log(todo);
+    //     for (let check of checkbox) {
+    //         todo.isDone = check.checked === true;
+    //     }
+    //     console.log(todo.isDone);
+    // });
+
+    return newArr;
+}
+
+
 /* MAKE NEW TODOS */
 let makeNewTodo = todoItem => {
     let todo = document.createElement('div');
-
-    todo.innerText = todoItem.title;
-    todo.className = 'newTodo';
+        todo.innerText = todoItem.title;
+        todo.className = 'newTodo';
+        todo.id = todoItem.id;
 
     let checkbox = todo.appendChild(document.createElement('input'));
-    checkbox.type = 'checkbox';
+        checkbox.type = 'checkbox';
+        checkbox.className = 'checkbox';
+        checkbox.id = todoItem.id;
+
+    checkbox.addEventListener('change', checkStat);
+    // if (localStorage.getItem('checkbox') === 'true') {
+    //     checkbox.setAttribute('checked', 'checked');
+    // }
 
     let remove = todo.appendChild(document.createElement('button'));
-    remove.innerText = 'clear';
+        remove.innerText = 'clear';
+    remove.addEventListener('click', removeItem);
 
     todosList.appendChild(todo);
 };
+
+
+/* REMOVE ITEM */
+function removeItem() {
+    // console.log(this.parentElement);
+    // let id = this.parentElement.id;
+    // concatTodos.splice(id, 1);
+    // localStorage.setItem('todos', JSON.stringify(concatTodos));
+}
 
 
 /* ADD NEW TODOS */
@@ -61,16 +117,19 @@ addTodo.addEventListener('click', function () {
     let newTodoName = newTodo.value;
     let maxId = getMaxId(concatTodos);
 
+    if (newTodo.value === '') {
+        return false;
+    } // ease validation
+
     let todo = {
         isDone: false,
         id: maxId,
         title: newTodoName,
     };
     concatTodos.push(todo);
-
-    localStorage.setItem('todos', JSON.stringify(concatTodos));
     makeNewTodo(todo);
     newTodo.value = '';
+
 });
 
 
@@ -78,3 +137,7 @@ concatTodos.forEach(todoItem => {
     makeNewTodo(todoItem);
 });
 
+document.getElementById('clear')
+    .addEventListener('click', function () {
+       localStorage.clear();
+    });
